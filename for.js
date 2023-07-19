d46fad67aede42de89ab431802ed0af8a44b08bd
Esteban <estebanvelascoriano@gmail.com>
2023-07-18 22:47:32 -0500

    tercer ejercicio

---
+++ for.js
@@ -0,0 +1,14 @@
+//variables
+let dato = 10; 
+let resultado = 10;  
+let array;
+//bucle
+for (let i = 10; i >= 1  ; i--) {
+    const element = i;
+    //condicional para que no se vuelva un mierdero
+    if (i == 1){
+        break
+    }   
+    console.log(element)
+}
+
---
+++ while.js
@@ -0,0 +1,13 @@
+function factorial(numero){
+    let resultado = 1;
+    let i = numero;
+    while(i <= numero ){
+        resultado = numero*1     
+        if(i<=numero){
+            --i
+            return resultado
+        }   
+        break   
+    }
+}
+console.log(factorial(5))
